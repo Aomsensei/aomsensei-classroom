bfc9e0223e7d38ff91ad074e2617c593bdda8be6
Aomsensei <shisong.comed@gmail.com>
2026-06-08 14:02:40 +0700

    Create config.js

---
+++ js/config.js
@@ -0,0 +1,22 @@
+// ====================================================
+// ⚙️ Firebase Configuration
+// กรุณาแทนที่ค่าด้านล่างด้วย Firebase Config ของคุณ
+// ดูขั้นตอนใน README.md
+// ====================================================
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAYFY_9VqMFsVX22EW2aPVq7NawWQV-n_g",
+  authDomain: "aomsensei-classroom.firebaseapp.com",
+  projectId: "aomsensei-classroom",
+  storageBucket: "aomsensei-classroom.firebasestorage.app",
+  messagingSenderId: "152996049271",
+  appId: "1:152996049271:web:a9a2927490e1e908d9f172"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
